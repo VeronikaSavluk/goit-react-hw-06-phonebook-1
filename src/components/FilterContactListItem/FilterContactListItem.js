f703a3a2d0e291494f66ab5577af69bc9d7d6f77
Veronika Savluk <s_veronica@ukr.net>
2022-10-24 03:09:43 +0300

    Update FilterContactListItem.js

--- src/components/FilterContactListItem/FilterContactListItem.js
+++ src/components/FilterContactListItem/FilterContactListItem.js
@@ -1,8 +1,6 @@
 import PropTypes from "prop-types";
-import { useSelector, useDispatch } from "react-redux";
-import { toggleCompleted, setStatusFilter } from "../../redux/actions";
-import { getStatusFilter } from "../../redux/selectors";
-import { statusFilters } from "../../redux/constants";
+import { useDispatch } from "react-redux";
+import { setFilter } from "../../redux/actions";
 import { nanoid } from "nanoid";
 
 import { ContactBook, NameInput } from './FilterContactListItem.styled';
@@ -12,28 +10,27 @@ const FilterContactList = () => {
     const QueryInputId = nanoid();
     const dispatch = useDispatch();
 
-    const filter = useSelector(getStatusFilter);
-    const query = useSelector(state => state.some.query);
+    const handleChange = e => {
+        e.preventDefault();
+        const form = e.target;
+        dispatch(setFilter(form.value.toLowerCase()));
+    };
 
-    const handleFilterChange = query => dispatch(setStatusFilter(query));
-const handleToggle = () => dispatch(toggleCompleted(contact.id));
     return (
         <ContactBook>
             <NameLable htmlFor={QueryInputId}>Find contacts by name</NameLable>
             <NameInput
                 type="text"
-                name="query"
-                value={query}
+                name="value"
                 id={QueryInputId}
-                onChange={onChange}
+                onChange={handleChange}
         />
         </ContactBook>
     )
 }
 
 FilterContactList.propTypes = {
-    query: PropTypes.string.isRequired,
-    onChange: PropTypes.func,
+    handleChange: PropTypes.func,
     QueryInputId: PropTypes.func,
 }
 

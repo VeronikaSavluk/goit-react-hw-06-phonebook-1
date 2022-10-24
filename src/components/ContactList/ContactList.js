48ed3e413d4c8673f44102949419980d614e07d8
Veronika Savluk <s_veronica@ukr.net>
2022-10-24 03:09:30 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,30 +1,20 @@
 import PropTypes from 'prop-types';
-import { useSelector } from "react-redux";
-import { statusFilters } from "../../redux/constans";
-import { getContacts, getStatusFilter } from 'redux/selectors';
+import { useSelector, useDispatch } from "react-redux";
+import { deleteContact } from 'redux/actions';
+import { getContacts } from 'redux/selectors';
 import { List, ContactItem, DataContact, Delete } from './ContactList.styled';
-
-const getVisibleContacts = (contacts, statusFilter) => {
-  switch (statusFilter) {
-    case statusFilters.active:
-      return contacts.filter(contact => contact.active);
-    default:
-      return contacts;
-  }
-};
 
 const ContactList = () => {
     const contacts = useSelector(getContacts);
-    const statusFilter = useSelector(getStatusFilter);
-    const visibleContacts = getVisibleContacts(contacts, statusFilter);
+    const dispatch = useDispatch();
 
     return (
         <List>
-            {visibleContacts.map(({id, name, number}) => (
+            {contacts.map(({id, name, number}) => (
                 <ContactItem key={id}>
                     <DataContact>{name}:</DataContact>
                     <DataContact>{number}</DataContact>
-                    <Delete onClick={() => onDeleteContact(id)}>Delete</Delete>
+                    <Delete onClick={() => dispatch(deleteContact(id))}>Delete</Delete>
                 </ContactItem>
             ))}
         </List>
@@ -37,7 +27,6 @@ ContactList.propTypes = {
             name: PropTypes.string.isRequired,
             number: PropTypes.string.isRequired,
             id: PropTypes.string,
-            onDeleteContact: PropTypes.func,
         }),
     )
 }
